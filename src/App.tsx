82ffd33798ba2cea0acc668c72c016c83e163ff7
Muratcan Akcay <muratcanakcay@gmail.com>
2021-11-10 00:56:20 +0100

    window open/close implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const App: React.FC = () => {
+
+  // DEFAULTS
+  const defaultSearchCount = 3
+  const defaultWaitInterval = 1 // seconds
+  // -----------
+
+  let openedWindow: Window
+
+  const [searchCount, setSearchCount] = useState(defaultSearchCount)
+
+  const openWindow = () => {
+    let newWindow = window.open("https://www.google.com", "_blank")
+    if (newWindow) openedWindow = newWindow
+
+    setTimeout(openedWindow.close, defaultWaitInterval * 1000)
+  }
+
+  const countChanged = (e: any) => {
+    setSearchCount(e.target.value)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container mt-3">
+      <div className="row my-5 d-inline-flex flex-row justify-content-start align-items-center">
+        <div className="col-md-auto">
+          <button className="btn btn-dark" onClick={openWindow}>START</button>
+        </div>
+        <div className="col">
+          <label htmlFor="searchCount">Number of searches:</label>
+        </div>
+        <div className="col">
+          <input
+            id="searchCount"
+            name="searchCount"
+            type="number"
+            value={searchCount}
+            max="100"
+            step="5"
+            required={true}
+            onChange={(e) => countChanged(e)} />
+        </div>
+      </div>
+    </div >
   );
 }
 
