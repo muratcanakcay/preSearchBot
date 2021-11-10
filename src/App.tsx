3f87d06a8ea87ab15f5860b435a2988d6ecdea69
Muratcan Akcay <muratcanakcay@gmail.com>
2021-11-10 01:43:49 +0100

    counter working

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,76 @@
 import React, { useState } from 'react';
 
+// You must allow pop-ups from localhost:3000
+
 const App: React.FC = () => {
 
-  // DEFAULTS
-  const defaultSearchCount = 3
-  const defaultWaitInterval = 1 // seconds
-  // -----------
+    // DEFAULTS
+    const defaultSearchCountTarget = 2
+    const defaultWaitBeforeClose = 2 // seconds
+    const defaultWaitBetweenSearches = 5 // seconds (must be greater than defaultWaitBeforeClose)
+    // -----------
 
-  let openedWindow: Window
+    let openedWindow: Window
+    let intervalId: any
+    let totalSearchCount: number = 0
 
-  const [searchCount, setSearchCount] = useState(defaultSearchCount)
+    const [searchCountTarget, setSearchCountTarget] = useState(defaultSearchCountTarget)
+    const [searchesConducted, setSearchesConducted] = useState(0)
 
-  const openWindow = () => {
-    let newWindow = window.open("https://www.google.com", "_blank")
-    if (newWindow) openedWindow = newWindow
+    const countChanged = (e: any) => {
+        setSearchCountTarget(e.target.value)
+    }
 
-    setTimeout(openedWindow.close, defaultWaitInterval * 1000)
-  }
+    const openWindow = () => {
+        console.log("Trying to open")
 
-  const countChanged = (e: any) => {
-    setSearchCount(e.target.value)
-  }
+        let newWindow = window.open("https://www.google.com")
+
+        if (newWindow) {
+            setSearchesConducted(++totalSearchCount)
+            if (totalSearchCount == searchCountTarget)
+                clearInterval(intervalId)
+
+            openedWindow = newWindow
+            setTimeout(closeWindow, defaultWaitBeforeClose * 1000)
+        }
+    }
+
+    const closeWindow = () => {
+        console.log("Trying to close")
+        openedWindow.close()
+    }
+
+    const startSearches = () => {
+        intervalId = setInterval(openWindow, defaultWaitBetweenSearches * 1000)
+    }
 
 
-  return (
-    <div className="container mt-3">
-      <div className="row my-5 d-inline-flex flex-row justify-content-start align-items-center">
-        <div className="col-md-auto">
-          <button className="btn btn-dark" onClick={openWindow}>START</button>
-        </div>
-        <div className="col">
-          <label htmlFor="searchCount">Number of searches:</label>
-        </div>
-        <div className="col">
-          <input
-            id="searchCount"
-            name="searchCount"
-            type="number"
-            value={searchCount}
-            max="100"
-            step="5"
-            required={true}
-            onChange={(e) => countChanged(e)} />
-        </div>
-      </div>
-    </div >
-  );
+    return (
+        <div className="container mt-3">
+            <div className="row my-5 d-inline-flex flex-row justify-content-start align-items-center">
+                <div className="col-md-auto">
+                    <button className="btn btn-dark" onClick={startSearches}>START</button>
+                </div>
+                <div className="col">
+                    <label htmlFor="searchCount">Number of searches:</label>
+                </div>
+                <div className="col-auto">
+                    <input
+                        id="searchCount"
+                        name="searchCount"
+                        type="number"
+                        value={searchCountTarget}
+                        max="100"
+                        step="5"
+                        required={true}
+                        onChange={(e) => countChanged(e)}
+                    />
+                    Searches conducted: {searchesConducted}
+                </div>
+            </div>
+        </div >
+    );
 }
 
 export default App;
